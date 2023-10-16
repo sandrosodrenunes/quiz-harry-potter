a7d61e8ff6eea1501556c7432d05189c996be78a
Sandro Sodré Nunes <sandrojunior802@gmail.com>
2023-10-16 17:25:30 -0300

    reformado

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const correctAnswers = ['A','B','C','B']
 const showScore = document.querySelector('.result')
 let score = 0;
 
-const setIntervalCounter = () => {
+
+// incrementa a pontuação do usuário no DOM
+const ShowUserScore  = () => {
     let counter = 0
-    
+
     const intervalId = setInterval(() => {
         
         if(counter === score || score === 0){
@@ -18,11 +20,11 @@ const setIntervalCounter = () => {
 
     }, 10)
 
-    scrollTo(0, 0);
 }
 
-const scoreSumUser = () => {
-    
+// obtém as respostas do usuário
+
+const getAnswersUser = () => {
     const userAnswers = [
         form.inputQuestion1.value,
         form.inputQuestion2.value,
@@ -30,6 +32,11 @@ const scoreSumUser = () => {
         form.inputQuestion4.value,
     ]
 
+    return userAnswers
+}
+
+const calculatesUserScore = (userAnswers) => {
+    
     userAnswers.forEach((userAnswer, index) => {
         if(userAnswer === correctAnswers[index]){
             score += 25
@@ -40,12 +47,19 @@ const scoreSumUser = () => {
 
 form.addEventListener('submit', event => {
     event.preventDefault()
-    score = 0
 
+    score = 0
+    scrollTo(0, 0);
     showScore.classList.remove('d-none');
 
-    scoreSumUser()
-    setIntervalCounter()
+    // obtém as respostas do usuário
+    const userAnswers = getAnswersUser()
+
+    // calcula a apontuação do usuário
+    calculatesUserScore(userAnswers)
+
+    // incrementa a pontuação do usuário no DOM
+    ShowUserScore()
 })
 
 
